aa4753daa5a97c40496f6553d5e7c3ed928818e6
pthomison <p.thomison@gmail.com>
2022-04-26 14:29:29 -0700

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const Web3 = require('web3');
+const AWSHttpProvider = require('@aws/web3-http-provider');
+
+ENDPOINT = "https://nd-bwruezapmrayxnzb2jogcqn2ei.ethereum.managedblockchain.us-east-1.amazonaws.com"
+
+// var web3 = new Web3('http://127.0.0.1:8545')	
+var web3 = new Web3(new AWSHttpProvider(ENDPOINT))
+
+
+async function main(exitCallback) {
+
+	let block = await web3.eth.getBlock("latest")
+	console.log(block);
+
+	console.log(await web3.eth.getNodeInfo());
+
+	exitCallback()
+}
+
+main(function(){
+	process.exit(0);
+})
